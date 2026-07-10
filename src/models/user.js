3d70bf895ec3935a13f94b173b62c7f379eca20c
Yugam Grover <yugamgrover13@gmail.com>
2026-07-10 16:03:47 +0530

    Implemented server-side sorting, filtering and pagination to tasks route

--- src/models/user.js
+++ src/models/user.js
@@ -4,50 +4,55 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Task = require("./task");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value))
-        throw new Error("Please enter a valid Email.");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if (value.includes("password")) {
-        throw new Error("use a strong password.");
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) throw new Error("Please enter a valid age.");
-    },
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value))
+          throw new Error("Please enter a valid Email.");
       },
     },
-  ],
-});
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 7,
+      validate(value) {
+        if (value.includes("password")) {
+          throw new Error("use a strong password.");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) throw new Error("Please enter a valid age.");
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
 
 userSchema.virtual("tasks", {
   ref: "Task",
--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -4,10 +4,29 @@ const auth = require("../middleware/auth");
 const router = new express.Router();
 
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+  if (req.query.completed) match.completed = req.query.completed === "true";
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split("_");
+    if (parts[1] === "desc" || parts[1] === "asc") {
+      sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+    }
+  }
+
   try {
-    await req.user.populate("tasks");
+    await req.user.populate({
+      path: "tasks",
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      },
+    });
     res.send(req.user.tasks);
-  } catch (error) {
+  } catch (e) {
     res.status(500).send();
   }
 });
