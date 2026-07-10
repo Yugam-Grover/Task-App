7564287b823b5a99a3eee877c125fef69b9e84ef
Yugam Grover <yugamgrover13@gmail.com>
2026-07-10 18:43:30 +0530

    implemented crud management via routes for user avatar using multer and sharp npm modules

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
+const multer = require("multer");
+const sharp = require("sharp");
+
 const router = new express.Router();
 
 router.get("/users/me", auth, (req, res) => {
@@ -78,6 +81,56 @@ router.delete("/users/me", auth, async (req, res) => {
   } catch (error) {
     res.status(500).send();
   }
+});
+
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("only jpg, jpeg and png supported."));
+    }
+    cb(null, true);
+  },
+});
+
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send();
+  }
+});
+
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  },
+);
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
 });
 
 module.exports = router;
